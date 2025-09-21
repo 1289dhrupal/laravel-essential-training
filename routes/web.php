8f560a40f47253244dcddbdf314d852ad14e870a
Dhrupal Shah <31788924+1289dhrupal@users.noreply.github.com>
2025-09-21 13:12:58 +0000

    Solution

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Student;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,4 +12,34 @@ Route::get('/', function () {
 
     // Dump the result
 
+    // 1. Using raw SQL queries
+    // $users = DB::select('select * from users');
+    // dd($users);
+
+    // 2. Using Query Builder
+    // $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    // dd($users);
+
+    // 3. Using Eloquent ORM
+    // $students = Student::all();
+    // dd($students);
+    // foreach ($students as $student) {
+    //     echo $student->name . ' - ' . $student->email . '<br>';
+    // }
+
+    // $student = new Student();
+    // $student->name = 'John Doe';
+    // $student->email = 'jad4@student.le.ac.uk';
+    // $student->save();
+
+    // $students = Student::select(['name', 'email'])->whereNotNull('email')->get();
+    // dd($students);
+
+    $student = Student::all()->where('email', 'jad4@student.le.ac.uk')->first();
+    // update only if student found and status column exists and status is not active
+    if ($student && $student->status !== 'active') {
+        $student->status = 'active';
+        $student->save();
+    }
+    dd($student);
 });
