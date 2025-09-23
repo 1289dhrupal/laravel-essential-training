35ece0785b602ae0f19cf2919b3edb046a84cb34
Dhrupal Shah <31788924+1289dhrupal@users.noreply.github.com>
2025-09-23 13:29:52 +0000

    04_04e

---
+++ resources/views/notes/create.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Notes
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg max-w-2xl">
+                <form action="{{ route('notes.store') }}" method="POST">
+                    @csrf
+                    <x-text-input name="title" placeholder="Title" class="w-full mb-4" />
+                    <x-textarea name="text" placeholder="Your note here..." rows="8" value=""
+                        class="w-full mt-6" />
+                    <x-primary-button class="mt-6">Save Note</x-primary-button>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
