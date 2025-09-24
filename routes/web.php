6f4dfded0158e4fdbef2394f8781b3646e6cd03a
Dhrupal Shah <31788924+1289dhrupal@users.noreply.github.com>
2025-09-24 13:03:59 +0100

    Working branch 02 (#3)
    * Handling routing begins
    
    * Handling Routing ends
    
    * Creating a view ends
    
    * Building layouts with Blade
    
    * Building layouts with blade
    
    * Working with controllers
    
    * Working with controllers
    
    * Solution: Create a controller, a view and add a route
    
    * Understand Laravel configuration
    
    * Understand Laravel configuration
    
    * Understand Laravel configuration
    
    * Understand Laravel configuration
    
    * Understand Laravel configuration
    
    * Working with the database
    
    * Creating and Running migrations
    
    * Creatig models and using Eloquent
    
    * Challenge to update a column
    
    * Spelling corrected
    
    * Solution to add new column
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Solution
    
    * fixed query
    
    ---------
    
    Co-authored-by: Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
    Co-authored-by: smoser-LiL <smoser-LiL@localhost>

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,51 @@
 <?php
 
-use App\Http\Controllers\GreetingController;
-use App\Http\Controllers\WelcomeController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', function () {
+    // // Get the student model where email is john@example.com
+    // $student = Student::where('email', 'john@example.com')->first();
 
-Route::get('/home', function () {
-    return view('home');
+    // // Update the 'status' column with the value 'active'
+    // $student->status = 'active';
+
+    // // Save the model
+    // $student->save();
+    // dd($student);
+
+    // Dump the result
+
+    // 1. Using raw SQL queries
+    // $users = DB::select('select * from users');
+    // dd($users);
+
+    // 2. Using Query Builder
+    // $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    // dd($users);
+
+    // 3. Using Eloquent ORM
+    // $students = Student::all();
+    // dd($students);
+    // foreach ($students as $student) {
+    //     echo $student->name . ' - ' . $student->email . '<br>';
+    // }
+
+    // $student = new Student();
+    // $student->name = 'John Doe';
+    // $student->email = 'jad4@student.le.ac.uk';
+    // $student->save();
+
+    // $students = Student::select(['name', 'email'])->whereNotNull('email')->get();
+    // dd($students);
+
+    $student = Student::where('email', 'jad4@student.le.ac.uk')->first();
+    // update only if student found and status column exists and status is not active
+    if ($student && $student->status !== 'active') {
+        $student->status = 'active';
+        $student->save();
+    }
+    dd($student);
 });
 
 Route::get('/about', function () {
