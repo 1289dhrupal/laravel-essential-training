54d14444c4e2e16989ce9b919bf69d8784cd960f
Dhrupal Shah <31788924+1289dhrupal@users.noreply.github.com>
2025-09-24 13:07:25 +0100

    Working branch 03 (#4)
    * Handling routing begins
    
    * Handling Routing ends
    
    * Creating a view ends
    
    * Building layouts with Blade
    
    * Solution: Create new resource
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * fixed app_url
    
    * solved
    
    ---------
    
    Co-authored-by: Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
    Co-authored-by: smoser-LiL <smoser-LiL@localhost>

--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,24 @@
 <?php
 
-use App\Models\Student;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\NotebookController;
 
 Route::get('/', function () {
-    // // Get the student model where email is john@example.com
-    // $student = Student::where('email', 'john@example.com')->first();
+    return "<h1>Welcome</h1>";
+});
 
-    // // Update the 'status' column with the value 'active'
-    // $student->status = 'active';
-
-    // // Save the model
-    // $student->save();
-    // dd($student);
-
-    // Dump the result
-
-    // 1. Using raw SQL queries
-    // $users = DB::select('select * from users');
-    // dd($users);
-
-    // 2. Using Query Builder
-    // $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
-    // dd($users);
-
-    // 3. Using Eloquent ORM
-    // $students = Student::all();
-    // dd($students);
-    // foreach ($students as $student) {
-    //     echo $student->name . ' - ' . $student->email . '<br>';
-    // }
-
-    // $student = new Student();
-    // $student->name = 'John Doe';
-    // $student->email = 'jad4@student.le.ac.uk';
-    // $student->save();
-
-    // $students = Student::select(['name', 'email'])->whereNotNull('email')->get();
-    // dd($students);
-
-    $student = Student::where('email', 'jad4@student.le.ac.uk')->first();
-    // update only if student found and status column exists and status is not active
-    if ($student && $student->status !== 'active') {
-        $student->status = 'active';
-        $student->save();
-    }
-    dd($student);
+Route::get('/home', function () {
+    return view('home', ['name' => "Shruti"]);
 });
 
 Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/hello', [GreetingController::class, 'index']);
+require __DIR__ . '/auth.php';
+
+Route::resource('notes', NoteController::class)->middleware('auth');
+
+Route::resource('notebooks', NotebookController::class)->middleware('auth');
