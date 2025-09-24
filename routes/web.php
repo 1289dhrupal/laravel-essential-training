74cab887f199babca9412a721e38d1eeb0b13199
Dhrupal Shah <31788924+1289dhrupal@users.noreply.github.com>
2025-09-24 13:01:03 +0100

    Working branch 01 (#2)
    * Handling routing begins
    
    * Handling Routing ends
    
    * Handling routing ends
    
    * Creating a view ends
    
    * Building layouts with Blade
    
    * Building layouts with blade
    
    * Working with controllers
    
    * Working with controllers
    
    * Working with controllers
    
    * Solution: Create a controller, a view and add a route
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * Moving files  using main
    
    * updated env
    
    * added controller
    
    * added learning.txt
    
    * solution 01_challenge
    
    * formatting
    
    ---------
    
    Co-authored-by: Shruti Balasa <69137210+shrutibalasawebdev@users.noreply.github.com>
    Co-authored-by: smoser-LiL <smoser-LiL@localhost>

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,17 @@
 <?php
 
-use App\Http\Controllers\NoteController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GreetingController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::get('/home', function () {
+    return view('home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/about', function () {
+    return view('about');
 });
 
-require __DIR__ . '/auth.php';
-
-Route::resource('notes', NoteController::class)->middleware('auth');
+Route::get('/hello', [GreetingController::class, 'index']);
